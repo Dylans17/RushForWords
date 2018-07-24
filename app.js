73864eb0ad4956e43ad467694adc4e2c842f7962
Dylans17 <Dylans2499@gmail.com>
2018-07-24 02:50:01 -0400

    Initial Working Build

---
+++ app.js
@@ -0,0 +1,147 @@
+"use strict";
+//netsh wlan start hostednetwork
+//starts a local network that should share on 192.168.137.1
+
+//call libraries
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+let storage = require('node-persist');
+//variables for app
+const gameLength = 30;
+let givenLettersArray = [];
+let validWordsArray = [];
+let currentGivenLetters = "";
+let currentValidWords = [];
+let userArray = [];
+//global.userArray = userArray;
+(async () => {
+await storage.init()
+givenLettersArray = await storage.getItem('letters')
+validWordsArray = await storage.getItem('words')
+console.log(givenLettersArray.length,validWordsArray.length);
+})()
+let Time = gameLength
+let delay = 5
+let delayTimeout = 0
+let countdownTimeout = 0
+let userCount = 0;
+app.get('/',function(req, res){
+  res.sendFile(__dirname + '/http/index.html');
+});
+app.use(express.static('http',{index:false,extensions:['html']}));
+http.listen(8080,function(){
+  console.log('Server started on port ' + 8080)
+})
+io.on('connection', function(socket){
+  console.log(socket.handshake.query.name + ' connected User Count:'+ ++userCount);
+  userArray.push(socket.handshake.query.name,0,[],false)
+  socket.on('disconnect', function(){
+    let index = userArray.indexOf(socket.handshake.query.name);
+    if (index > -1) {
+      userArray.splice(index, 4);
+  }
+    console.log(socket.handshake.query.name + ' disconnected User Count:'+ --userCount);
+  });
+  socket.on('submitWord', function(msg){
+    console.log(socket.handshake.query.name + ': ' + msg);
+    let index = userArray.indexOf(socket.handshake.query.name);
+    if (index > -1) {
+      userArray[index+1] += msg.length - 2 - (socket.handshake.query.name == 'Cassandra');
+      userArray[index+2].push(msg);
+    }
+    else {
+      console.log(";Tried to get ID of non-existent user",userArray,socket.handshake.query.name);
+    }
+    socket.broadcast.emit('submitWord', msg);
+    Time += msg.length - 2;
+  });
+  socket.on('wordsGenerated',function(givenLetters,validWords){
+    addtoArray(givenLetters,validWords);
+  });
+  socket.on('sendReady',function(ready){
+    console.log(socket.handshake.query.name+': '+ !!ready);
+    let index = userArray.indexOf(socket.handshake.query.name);
+    if (index > -1) {
+      userArray[index+3] = !!ready; //!! ensures that input is boolean
+    }
+    else {
+      console.log(";Tried to get ID of non-existent user",userArray,socket.handshake.query.name);
+    }
+    let allReady = true
+    for (let i = 0;i<userArray.length;i+=4) {
+      allReady = allReady && userArray[i+3]
+    }
+    console.log("All Ready :" + allReady)
+    if (allReady) {
+      delay = 5
+      startGame()
+    }
+    else {
+      clearTimeout(delayTimeout)
+      clearTimeout(countdownTimeout)
+      console.log("GAME NOT READY");
+      io.emit('delay',"Cancelled");
+    }
+  });
+});
+function startGame() {
+  console.log('GAME STARTING');
+  countdownTimeout = setTimeout(function() {
+    io.emit('startGame',currentGivenLetters = givenLettersArray.pop(),currentValidWords = validWordsArray.pop());
+    updateStorage();
+    Time = gameLength;
+    clearTimeout(countdownTimeout)
+    countdown()
+  },5000)
+  delayTimer()
+}
+function delayTimer() {
+  if (delay > 0) {
+    delayTimeout = setTimeout(delayTimer,1000);
+    io.emit('delay',delay--);
+  }
+}
+function countdown() {
+  if (Time > 0) {
+    countdownTimeout = setTimeout(countdown,1000);
+    io.emit('time',Time--);
+  }
+  else {
+    io.emit('time',Time--);
+    Time = gameLength;
+    console.log("GAME OVER");
+    for (let i = 0;i<userArray.length;i+=4) {
+      console.log(userArray[i]+": "+userArray[i+1] + "   " +userArray[i+2].length+" words")
+      userArray[i+1] = 0
+      userArray[i+2] = []
+      userArray[i+3] = false;
+    }
+  }
+}
+function ding() {
+  setTimeout(ding,2000)
+  io.emit('ding')
+}
+ding()
+function addtoArray(givenLetters,validWords) {
+  givenLettersArray.push(givenLetters)
+  validWordsArray.push(validWords)
+  updateStorage()
+}
+async function updateStorage () {
+  if (givenLettersArray.length == validWordsArray.length) {
+    console.log("Array length: "+givenLettersArray.length)
+    await storage.setItem('letters',givenLettersArray)
+    await storage.setItem('words',validWordsArray)
+  }
+  else {
+    console.log("givenLetters and validWords arrays have different lengths")
+    let minLength = Math.min(givenLettersArray.length,validWordsArray.length)
+    givenLettersArray.splice(minLength);
+    validWordsArray.splice(minLength);
+    updateStorage();
+  }
+}
---
+++ http/functions.js
@@ -0,0 +1,192 @@
+"use strict";
+let validWords = [];
+let foundWords = [];
+let wordsIfound = [];
+let givenLetters = "";
+let activeLetter = 0;
+let activeLetters = "";
+let activeLettersIndex = '';
+let score = 0;
+let ready = false;
+function createGame() {
+    createLetters();
+    detectWords();
+    if (validWords.length > 100 && validWords[validWords.length-1].length >= 8){
+        sort(validWords);
+        givenLetters = mixLetters(givenLetters);
+        console.log(++score)
+        socket.emit('wordsGenerated',givenLetters,validWords);
+    }
+    requestAnimationFrame(createGame);
+    return;
+}
+function createLetters() {
+  givenLetters = ""
+  const vowelCount = Math.round(Math.random()*3)+2
+  for (let i = 0;i < 10-vowelCount;i++) {
+    let a = Math.round(Math.random() * Math.random() * 19)
+      givenLetters += consonants[a];
+  }
+  for (let i = 0;i < vowelCount;i++) {
+    let a = Math.round(Math.random() * 4.7)
+    //if (givenLetters.indexOf(vowels[a]) == -1)
+      givenLetters += vowels[a];
+    //else i--;
+  }
+}
+function startGame() {
+  foundWords = [];
+  wordsIfound = [];
+  activeLetter = 0;
+  activeLetters = "";
+  activeLettersIndex = "";
+  score = 0;
+  document.getElementById("spot" + 9).className = "letter blank";
+  document.getElementById('score').innerHTML = score
+  document.getElementById("wordsIfoundList").innerHTML = ""
+  populateLetters(" ");
+  document.getElementById("spot" + 9).removeEventListener("click", toggleReady);
+  addClickEvents();
+}
+function detectWords() {
+    validWords = [];
+    for (let i = 0;i<wordList.length;i++) {
+        let letterList = givenLetters
+        for (let p = 0;p <= wordList[i].length;p++) {
+            if (p == wordList[i].length) validWords.push(wordList[i]);
+            let newList = letterList.replace(wordList[i].charAt(p),"");
+            if (letterList == newList)break;
+            letterList = newList
+        }
+    }
+}
+function sort(array) {
+    array.sort(function(a, b) {
+        return a.length - b.length || a.localeCompare(b)
+    })
+}
+function populateLetters(resetOld) {
+    for (let i = 0;i<10;i++) {
+        document.getElementById("letter" + i).innerText = givenLetters.charAt(i).toUpperCase()
+    }
+    if (resetOld) {
+        for (let i = 0;i<10;i++) {
+            document.getElementById("spot" + i).innerText = resetOld.charAt(i)
+        }
+    }
+}
+function addClickEvents() {
+    for (let i = 0;i<10;i++) {
+        document.getElementById("letter" + i).addEventListener("click", placeLetter);
+        document.getElementById("spot" + i).addEventListener("click", submitWord);
+    }
+}
+function mixLetters(inputStr) {
+    return inputStr.split('').sort(function(a, b) {
+        return Math.random()>.5 ? -1 : 1;
+    }).join('')
+}
+function placeLetter(e) {
+  if (e.srcElement.innerText !== "") {
+    activeLetters += e.srcElement.innerText.toLowerCase()
+    activeLettersIndex += e.srcElement.id.charAt(6)
+    document.getElementById("spot" + activeLetter++).innerText = e.srcElement.innerText
+    e.srcElement.innerText = ""
+    checkClass()
+  }
+  //parseInt(e.srcElement.id.charAt(6)) //Get Element ID
+}
+function checkClass() {
+  if(validWords.indexOf(activeLetters)+1) {
+    if(foundWords.indexOf(activeLetters)+1) {
+      changeClass("used");
+    }
+    else {
+      changeClass("correct");
+    }
+  }
+  else {
+    changeClass("wrong");
+  }
+}
+function changeClass(newClass) {
+    for (let i = 0;i<activeLetter;i++) {
+        document.getElementById("spot" + i).className = "letter " + newClass;
+    }
+}
+function submitWord(e,char) {
+  if (activeLetters == "") {
+    givenLetters = mixLetters(givenLetters);
+    populateLetters();
+    return
+  }
+  if (activeLetter <= e.srcElement.id.charAt(4)) {
+    if(validWords.indexOf(activeLetters)+1 && !(foundWords.indexOf(activeLetters)+1)) {
+      foundWords.push(activeLetters);
+      wordsIfound.push(activeLetters);
+      socket.emit('submitWord',activeLetters)
+      score += activeLetters.length - 2 - (name == 'Cassandra')
+      document.getElementById('score').innerHTML = score
+    }
+    populateLetters(" ");
+    changeClass("blank");
+    activeLetter = 0;
+    activeLetters = "";
+    activeLettersIndex = "";
+    return;
+  }
+  else {
+    const missingLetter = e.srcElement.id.charAt(4)
+    const missingLetterOrigin = activeLettersIndex.charAt(missingLetter)
+    e.srcElement.innerHTML=""
+    document.getElementById("letter" + missingLetterOrigin).innerHTML = activeLetters.charAt(missingLetter).toUpperCase();
+    activeLetters = activeLetters.slice(0, missingLetter) + activeLetters.slice(parseInt(missingLetter)+1);
+    activeLettersIndex = activeLettersIndex.slice(0, missingLetter) + activeLettersIndex.slice(parseInt(missingLetter)+1);
+    for (let i = missingLetter;i<activeLetter;document.getElementById('spot' + i).innerHTML = activeLetters.charAt(i++).toUpperCase()){}
+    document.getElementById("spot" + --activeLetter).className = "letter blank";
+    checkClass();
+  }
+}
+function gameOver() {
+  changeClass("blank")
+  populateLetters("GAME OVER➠")
+  if (score > highScore) {
+    localStorage.setItem("High Score",score)
+  }
+  for (let i = 0;i<10;i++) {
+    document.getElementById("letter" + i).removeEventListener("click", placeLetter);
+    document.getElementById("spot" + i).removeEventListener("click", submitWord);
+  }
+  ready = false;
+  document.getElementById("spot" + 9).className = "letter wrong";
+  document.getElementById("spot" + 9).addEventListener("click", toggleReady);
+  sort(wordsIfound);
+  for (let i = 0;i<wordsIfound.length;i++) {
+    document.getElementById("wordsIfoundList").innerHTML += "<li>" + wordsIfound[i] + "</li>"
+  }
+}
+function toggleReady() {
+  ready = !ready
+  document.getElementById("spot" + 9).className = "letter " + (ready ? 'correct' : 'wrong');
+  socket.emit('sendReady',ready)
+}
+function noPing() {
+  document.getElementById('connection').style.color = "Red";
+  document.getElementById('connection').innerHTML = "Disconnect";
+}
+function changescreen(screen) {
+  document.getElementById("Screen0").style.display = (screen==0 ? 'block' : 'none');
+  document.getElementById("Screen1").style.display = (screen==1 ? 'block' : 'none');
+  document.getElementById("Screen2").style.display = (screen==2 ? 'block' : 'none');
+  document.getElementById("Screen3").style.display = (screen==3 ? 'block' : 'none');
+  if (screen == 3 && givenLetters == "") {
+    ready = false;
+    document.getElementById("spot" + 9).className = "letter wrong";
+    document.getElementById("spot" + 9).innerHTML = "➠";
+    document.getElementById("spot" + 9).addEventListener("click", toggleReady);
+  }
+}
+document.body.addEventListener("keydown", test);
+function test(e) {
+  //console.log(e.key)
+}
